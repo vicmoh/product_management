e5d986ba444b70e63d33d3c3c42d5207c5d0aea5
Vicky Mohammad <vimoh1213@gmail.com>
2018-07-24 21:40:21 -0400

    purple back button

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
-class ProductPage extends StatelessWidget{
+class ProductPage extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Product Detail")
-        ),
-        body: Center(
-          child: Column(children: <Widget>[
-            Text("Details"),
-            RaisedButton(child: Text('BACK'), 
-              onPressed: () => Navigator.pop(context),
-            )
-          ],)
-        ),
-      );
-    }//end build
-}//end class
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("Product Detail")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset("assets/food.jpg"),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text("Details"),
+          ),
+          Container(
+              child: RaisedButton(
+                color: Colors.purple,
+                child: Text('BACK', style: TextStyle(color: Colors.white),),
+                onPressed: () => Navigator.pop(context),
+              ),
+              padding: EdgeInsets.all(10.0)),
+        ],
+      ),
+    );
+  } //end build
+} //end class
