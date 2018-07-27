d47afb188fd72177b001ceb0a37b4cf1e88e2fa5
Vicky Mohammad <vimoh1213@gmail.com>
2018-07-27 17:56:35 -0400

    remoove hamburger on menu

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 import '.././product_manager.dart';
 
-class ProductsPage extends StatelessWidget{
+class ProductsPage extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // drawer
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(title: Text("Choose"),
+              // remove hamburger icon
+              automaticallyImplyLeading: false,
+            ),
+            ListTile(
+              title: Text("Manage Product"),
+              onTap: () {},
+            ),
+          ],
+        )
+      ),
 
-        // app bar
-        appBar: AppBar(
-          title: Text("Title"),
-          centerTitle: true,
-        ),
+      // app bar
+      appBar: AppBar(
+        title: Text("Title"),
+        centerTitle: true,
+      ),
 
-        // body
-        body: ProductManager(),
-
-      );
-    }
+      // body
+      body: ProductManager(),
+    );
+  }
 }
