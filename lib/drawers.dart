cedff5bd319adaefe550f74d52f2dd69a5cad0ca
Vicky Mohammad <vimoh1213@gmail.com>
2018-07-29 19:51:52 -0400

    organize

--- lib/drawers.dart
+++ lib/drawers.dart
@@ -3,41 +3,40 @@ import './pages/product_admin.dart';
 import './pages/products.dart';
 
 class MenuDrawer extends StatelessWidget{
-
-  Route _pageRouteTo(Widget thePage, BuildContext context){
-    return MaterialPageRoute(
-      builder: (context) =>thePage
-    );
-  }//end route
-
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(title: Text("Choose"),
-              // remove hamburger icon
-              automaticallyImplyLeading: false,
-            ),
-            ListTile(
-              title: Text("Manage Product"),
-              onTap: () {
-                Navigator.pushReplacement(context, 
-                  _pageRouteTo(ProductAdminPage(),context)
-                );
-              }, 
-            ),
-            ListTile(
-              title: Text("Products"),
-              onTap: () {
-                Navigator.pushReplacement(context, 
-                  _pageRouteTo(ProductsPage(), context)
-                );
-              }, 
-            ),
-          ],
-        )
+  Widget build(BuildContext context) {
+
+    // page routing
+    Route _pageRouteTo(Widget thePage){
+      return MaterialPageRoute(
+        builder: (context) =>thePage
       );
-    }
-}
+    }//end route
+
+    // menu button
+    _menuButton(String label, Widget page){
+      return ListTile(
+        title: Text(label),
+        onTap: () {
+          Navigator.pushReplacement(context, 
+            _pageRouteTo(page)
+          );
+        }, 
+      );
+    }//end func
+
+    // drawer
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(title: Text("Choose"),
+            // remove hamburger icon
+            automaticallyImplyLeading: false,
+          ),
+          _menuButton("Manage Product", ProductAdminPage()),
+          _menuButton("Products", ProductsPage())
+        ],
+      )
+    );
+  }//end build
+}//end classs
