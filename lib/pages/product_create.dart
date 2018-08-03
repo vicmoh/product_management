f35c4ace72b4df19b71014145f630b3a5726f4ab
Vicky Mohammad <vimoh1213@gmail.com>
2018-08-03 19:15:20 -0400

    textfield

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatefulWidget{
+  @override 
+  State <StatefulWidget> createState(){
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage>{
+
+  String titleValue = '';
+
+  _modalShowCase(BuildContext context){
+    return Center(
+      child: RaisedButton(
+        child: Text("save"),
+
+        // sliding modal from bottom
+        onPressed: () {
+          showModalBottomSheet(context: context,
+            builder: (BuildContext context){
+              return Center(child: Text("This is Modal!"));
+            }
+          );
+        }
+
+      ),
+    );
+  }
+
   @override
     Widget build(BuildContext context) {
       // TODO: implement build
-      return Center(
-        child: RaisedButton(
-          child: Text("save"),
-
-          // sliding modal from bottom
-          onPressed: () {
-            showModalBottomSheet(context: context,
-              builder: (BuildContext context){
-                return Center(child: Text("This is Modal!"));
-              }
-            );
-          }
-
-        ),
-      );
+      return Column(children: <Widget>[
+        TextField(onChanged: (String value){
+          setState(() {
+            titleValue = value;
+          });
+        },),
+        Text(titleValue),
+      ],);
     }
 }
