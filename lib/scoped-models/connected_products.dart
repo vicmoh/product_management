ef523fdbbde1dc41c042eb7a26afed266f5756fc
Vicky Mohammad <vimoh1213@gmail.com>
2018-10-09 00:08:57 -0400

    asd

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -152,7 +152,9 @@ class ProductsModel extends ConnectedProductsModel {
             image: productData['image'],
             price: productData['price'],
             userEmail: productData['userEmail'],
-            userId: productData['userId']);
+            userId: productData['userId'],
+            isFavorite: (productData['wishlistUsers'] as Map<String, dynamic>)
+              .containsKey(_authenticatedUser.id));
         fetchedProductList.add(product);
       });
       _products = fetchedProductList;
